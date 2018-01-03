9807498a9f951ac61383afd39e5579feace968de
Ilia Orlov <2x2.4raymon@gmail.ru>
2018-01-03 22:22:56 +0300

    Add simple info service

---
+++ info-service/src/main/java/com/base/spring/InfoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.base.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class InfoServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(InfoServiceApplication.class, args);
+    }
+}
