956bebfe6b499357b3e6ae5e82e1f51bcf870b74
Ilia Orlov <2x2.4@mail.ru>
2018-01-09 00:50:45 +0300

    Add simple zuul pre filter

---
+++ zuul-service/src/main/java/com/base/spring/filter/pre/SimplePreFilter.java
@@ -0,0 +1,39 @@
+package com.base.spring.filter.pre;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
+
+@Slf4j
+@Component
+public class SimplePreFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+        log.info(request.getMethod() + " request to " + request.getRequestURI());
+
+        return null;
+    }
+}
