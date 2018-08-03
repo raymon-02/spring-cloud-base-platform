50055e0556b09095425975dc1d2f0a7d0ffb3047
Ilia Orlov <2x2.4@mail.ru>
2018-08-03 16:09:44 +0300

    Fix hystrix-dashboard

--- calculate-service/src/main/java/com/base/spring/CalculateServiceApplication.java
+++ calculate-service/src/main/java/com/base/spring/CalculateServiceApplication.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @EnableEurekaClient
 @EnableCircuitBreaker
-@EnableHystrixDashboard
 @SpringBootApplication
 public class CalculateServiceApplication {
     public static void main(String[] args) {
